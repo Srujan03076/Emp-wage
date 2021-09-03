174d1912bb814f645e6bbd8ed153314d3b7589fa
unknown <srujanmahaja5@gmail.com>
2021-09-03 14:28:48 +0530

    Modified Empwage

---
+++ Empwage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EmployeeCompute058
+{
+    public class UC3_PartTimeEmp
+    {
+        static void Main(String[] args)
+        {
+            //UC3 wage
+            //Constants
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage : " + empWage);
+            Console.Read();
+        }
+    }
+}
